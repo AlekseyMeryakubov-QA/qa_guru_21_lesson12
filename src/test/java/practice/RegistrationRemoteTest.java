635904d663fb39e48e6f2d3491f702700aa717c7
ameryakubov <alex_meryakubov@mail.ru>
2023-08-27 22:24:44 +0300

    add remote test with step

--- src/test/java/practice/RegistrationRemoteTest.java
+++ src/test/java/practice/RegistrationRemoteTest.java
@@ -1,9 +1,9 @@
 package practice;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -13,6 +13,7 @@ import java.util.Map;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 import static org.asynchttpclient.util.HttpConstants.Methods.OPTIONS;
 
 public class RegistrationRemoteTest {
@@ -34,46 +35,51 @@ public class RegistrationRemoteTest {
         selenoidOptions.put("enableVideo", true);
         capabilities.setCapability("selenoid:options", selenoidOptions);
         Configuration.browserCapabilities = capabilities;
-
-
+    }
+    @BeforeEach
+    void addListener (){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @Test
     @Tag("remote")
+    @DisplayName("Successuful registration")
     void fillRegistrationRemoteFormTest() {
-        open("/automation-practice-form");
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Egorov");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").$(byText("2005")).click();
-        $(".react-datepicker__month-select").$(byText("July")).click();
-        $("div.react-datepicker__week div.react-datepicker__day--001").click();
-        $("#subjectsInput").setValue("History").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("image.png");
-        $("#currentAddress").setValue("New York");
-
-
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("Haryana")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Karnal")).click();
-        $("#submit").click();
-
-        $(".table-responsive").shouldHave(text("Alex Egorov"));
-        $(".table-responsive").shouldHave(text("alex@egorov.com"));
-        $(".table-responsive").shouldHave(text("Male"));
-        $(".table-responsive").shouldHave(text("1234567890"));
-        $(".table-responsive").shouldHave(text("01 July,2005"));
-        $(".table-responsive").shouldHave(text("Sports"));
-        $(".table-responsive").shouldHave(text("image.png"));
-        $(".table-responsive").shouldHave(text("New York"));
-        $(".table-responsive").shouldHave(text("Haryana Karnal"));
+        step("Open form",() -> {
+            open("/automation-practice-form");
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+        });
+        step("Fill form",() -> {
+            $("#firstName").setValue("Alex");
+            $("#lastName").setValue("Egorov");
+            $("#userEmail").setValue("alex@egorov.com");
+            $("#genterWrapper").$(byText("Male")).click();
+            $("#userNumber").setValue("1234567890");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__year-select").$(byText("2005")).click();
+            $(".react-datepicker__month-select").$(byText("July")).click();
+            $("div.react-datepicker__week div.react-datepicker__day--001").click();
+            $("#subjectsInput").setValue("History").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#uploadPicture").uploadFromClasspath("image.png");
+            $("#currentAddress").setValue("New York");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("Haryana")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Karnal")).click();
+            $("#submit").click();
+        });
+        step("Verify results", () -> {
+            $(".table-responsive").shouldHave(text("Alex Egorov"));
+            $(".table-responsive").shouldHave(text("alex@egorov.com"));
+            $(".table-responsive").shouldHave(text("Male"));
+            $(".table-responsive").shouldHave(text("1234567890"));
+            $(".table-responsive").shouldHave(text("01 July,2005"));
+            $(".table-responsive").shouldHave(text("Sports"));
+            $(".table-responsive").shouldHave(text("image.png"));
+            $(".table-responsive").shouldHave(text("New York"));
+            $(".table-responsive").shouldHave(text("Haryana Karnal"));
+        });
     }
 }
