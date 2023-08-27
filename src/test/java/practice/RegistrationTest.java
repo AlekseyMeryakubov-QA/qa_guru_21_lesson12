6c3918f5011921a6534095cffbe9776eb1040f02
ameryakubov <alex_meryakubov@mail.ru>
2023-08-27 21:12:41 +0300

    add remote test

---
+++ src/test/java/practice/RegistrationTest.java
@@ -0,0 +1,61 @@
+package practice;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = (10);
+    }
+
+    @Test
+    @Tag("remote")
+    void fillRegistrationFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Egorov");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").$(byText("2005")).click();
+        $(".react-datepicker__month-select").$(byText("July")).click();
+        $("div.react-datepicker__week div.react-datepicker__day--001").click();
+        $("#subjectsInput").setValue("History").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("image.png");
+        $("#currentAddress").setValue("New York");
+
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(text("Alex Egorov"));
+        $(".table-responsive").shouldHave(text("alex@egorov.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("1234567890"));
+        $(".table-responsive").shouldHave(text("01 July,2005"));
+        $(".table-responsive").shouldHave(text("Sports"));
+        $(".table-responsive").shouldHave(text("image.png"));
+        $(".table-responsive").shouldHave(text("New York"));
+        $(".table-responsive").shouldHave(text("Haryana Karnal"));
+    }
+}
